8949fbb34c44daee55ae005f86ecccef5ed10920
crsohara <chris.ohara@frantic.com>
2019-09-11 16:46:17 +0300

    Part5.8

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -3,13 +3,16 @@ import React from 'react'
 import Blog from './Blog'
 
 const Blogs = ({blogs, handleLike}) => (
-  blogs.map( (blog) => (
-    <Blog
-      blog={blog}
-      handleLike={() => handleLike(blog.id)}
-      key={blog.id}
-    />
-  ))
+
+  blogs
+    .sort( (a, b) => b.likes - a.likes )
+    .map( (blog) => (
+      <Blog
+        blog={blog}
+        handleLike={() => handleLike(blog.id)}
+        key={blog.id}
+      />
+    ))
 )
 
 export default Blogs
